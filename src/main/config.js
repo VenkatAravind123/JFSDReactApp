f8bd9a377514d4ceb53e17f879e6082e66687baf
Venkat Aravind <venkataravind.123456@gmail.com>
2024-12-08 04:19:02 +0530

    V4.0

--- src/main/config.js
+++ src/main/config.js
@@ -1,7 +1,7 @@
 const config = 
 {
     // "url":"http://localhost:2021"
-    "url":"https://janasevasdp25.up.railway.app/"
+    "url":"https://janasevasdp25.up.railway.app"
 }
 
 export default config;
